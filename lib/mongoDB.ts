dd457c89d79d457d5d641fe372bc130f49dc5843
Luis Eduardo Diaz Valle <126373850+DIVL7@users.noreply.github.com>
2024-08-25 23:05:10 -0600

    Name change

--- lib/mongoDB.ts
+++ lib/mongoDB.ts
@@ -12,7 +12,7 @@ export const connectToDB = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL || "", {
-      dbName: "Kos_Store"
+      dbName: "Kos_WebPage"
     })
 
     isConnected = true;
